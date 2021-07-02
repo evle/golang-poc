5b3c875e5d6034edc4344a0dc6ab29d11eabf37c
evle <evlefsp@163.com>
2021-07-02 21:57:12 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "io"
+    "log"
+    "net/http"
+	"encoding/json"
+)
+
+type JsonResult struct{
+	Code int `json:"code"`
+	Message string `json:"message"`
+}
+
+func handler(w http.ResponseWriter, r *http.Request)  {
+	ret:=new(JsonResult)
+	ret.Code = 0
+	ret.Message = "success"
+	msg, _ := json.Marshal(ret)
+	io.WriteString(w, string(msg))
+}
+
+func main() {
+    http.HandleFunc("/api/account/register", handler)    
+	http.HandleFunc("/api/account/login", handler)    
+    err := http.ListenAndServe(":3000", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
